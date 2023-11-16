522cd98f33f27ad2303b6152189031ddaf1b1d89
Oleg <olberd@bk.ru>
2023-11-16 20:40:22 +0300

    Add login.

--- storages/urls.py
+++ storages/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('faq/', views.faq, name='faq'),
     path('boxes/', views.boxes, name='boxes'),
     path('my_rent/', views.my_rent, name='my_rent'),
+    path('login/', views.login_user, name='login'),
 ]
--- storages/views.py
+++ storages/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
+from storages.forms import LoginForm
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd["email"], password=cd["password"])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("/")
+                else:
+                    form.add_error(None, ValidationError("Этот аккаунт отключен"))
+            else:
+                form.add_error(None, ValidationError("Неверный email или пароль."))
+    else:
+        form = LoginForm()
+    return render(request, "index.html", {"form": form})
 
 
 def index(request):
