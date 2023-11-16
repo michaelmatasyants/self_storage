c9743086922171921741745b6d86b9f3e65b4e5d
Michael Matasyants <michael.matasyanc@gmail.com>
2023-11-16 04:23:24 +0300

    Add templates and urls

--- storages/views.py
+++ storages/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    '''Main_page view'''
+    return render(request, 'index.html')
+
+
+def faq(request):
+    '''FAQ view'''
+    return render(request, 'faq.html')
+
+
+def boxes(request):
+    '''Boxes view'''
+    return render(request, 'boxes.html')
+
+
+def my_rent(request):
+    '''My rent view'''
+    return render(request, 'my-rent.html')
