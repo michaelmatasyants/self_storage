9f5b219482dea4b3fc29584095517f6092897aeb
Oleg <olberd@bk.ru>
2023-11-16 12:20:09 +0300

    Add models to admin.py.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from storages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('login/', views.login_user, name='login'),
+    # path('registration/', views.register_user, name='registration'),
+    # path('logout/', views.logout_user, name='logout'),
 ]
