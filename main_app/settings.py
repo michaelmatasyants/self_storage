ab9ec11369830f7b4c64795489ce225d09d6e964
Anna Veselova <anna.veselova.97@mail.ru>
2023-11-18 17:15:19 +0300

    add TINYMCE_DEFAULT_CONFIG

--- main_app/settings.py
+++ main_app/settings.py
@@ -91,6 +91,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
+TINYMCE_DEFAULT_CONFIG = {
+    'content_css': STATIC_ROOT,
+}
+
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
