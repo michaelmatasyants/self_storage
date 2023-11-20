88aa71f3921f8002cf13d5a56cc56c9fa4c05221
Oleg <olberd@bk.ru>
2023-11-20 12:21:38 +0300

    Move get_payment_link to funcs.py.

--- storages/funcs.py
+++ storages/funcs.py
@@ -1,7 +1,8 @@
 import os
 
-from main_app.settings import SELF_STORAGE_URL
-
+from main_app.settings import SELF_STORAGE_URL, YOOKASSA_SHOP_ID, YOOKASSA_SECRET_KEY
+import uuid
+from yookassa import Configuration, Payment
 
 def get_html_message(order: dict):
     filepath = os.path.join('messages', 'message.html')
@@ -21,6 +22,25 @@ def get_html_message(order: dict):
         message = message.replace(key, value)
 
     return message
+
+
+def get_payment_link(value, description):
+    Configuration.account_id = YOOKASSA_SHOP_ID
+    Configuration.secret_key = YOOKASSA_SECRET_KEY
+    payment = Payment.create({
+        "amount": {
+            "value": value,
+            "currency": "RUB"
+        },
+        "confirmation": {
+            "type": "redirect",
+            "return_url": f"{SELF_STORAGE_URL}"
+        },
+        "capture": True,
+        "description": description
+    }, uuid.uuid4())
+
+    return payment.confirmation.confirmation_url
 
 
 
